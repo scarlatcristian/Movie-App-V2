943f06de51e9a6dad34d4187f966f78fabf6a19a
cristi <scarlat.cristian.gabriel@gmail.com>
2023-06-03 14:55:21 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <button class="popular">What's popular</button>
+        <form id="form">
+          <input type="text" id="search" class="search" placeholder="search" />
+        </form>
+      </header>
+
+      <main id="main"></main>
+    </div>
+  );
+}
+
+export default App;
